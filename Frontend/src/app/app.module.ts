708f57462546d753e52aad1d66e8082b6de548e0
Gabrielgoc99 <gabrielgoc99@gmail.com>
2021-12-02 21:29:10 -0300

    Ajustes no navbar, footer e home.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CriacaoComponent } from './criacao/criacao.component';
 import { EdicaoComponent } from './edicao/edicao.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import { ListarComponent } from './listar/listar.component';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -15,12 +26,21 @@ import { EdicaoComponent } from './edicao/edicao.component';
     NavbarComponent,
     FooterComponent,
     CriacaoComponent,
-    EdicaoComponent
+    EdicaoComponent,
+    ListarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavbarComponent, FooterComponent, PrincipalComponent]
 })
 export class AppModule { }
